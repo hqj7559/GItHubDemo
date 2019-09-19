5a65707ff1c8055fa2c3bb4bcf0adbcb8b52b43e
吃喝玩乐睡 <@DESKTOP-OCBB04I>
2019-09-19 09:42:42 +0800

    好好学习 天天向上

--- src/test/app.java
+++ src/test/app.java
@@ -4,7 +4,7 @@ public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("µğÄãÂèµÄ£¡");
+		System.out.print("Good Good study Day Day up£¡");
 	}
 
 }
